6bbbaf35c745bc3f47e877c51f2388386251471d
pazukdev <pazukdev@gmail.com>
2019-12-27 15:35:21 +0300

    TextHandler: replaceCharAWithOAfterP() and replaceWordsWithSubstring() methods are added

--- src/com/pazukdev/Main.java
+++ src/com/pazukdev/Main.java
@@ -1,17 +1,52 @@
 package com.pazukdev;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        final String text =  "Bite my shiny metal ass";
+        final String text =  "Bite my shiny metal axe! And don't forget about my Lunapark!";
         final char replacer = 'X';
         final int indexOfCharToReplace = 2;
+        final int wordLengthToReplaceWithSubstring = 3;
+        final String substring = "ass";
+
+        System.out.println(text + " (original text)");
 
         TextHandler textHandler = new TextHandler();
-        final String handledText = textHandler.handle(text, replacer, indexOfCharToReplace);
 
-        System.out.println(text);
-        System.out.println(handledText);
+        // #1
+        List<String> words = textHandler.splitIntoWords(text);
+        textHandler.replaceChar(replacer, indexOfCharToReplace, words);
+        String handledText = textHandler.wordsIntoText(words);
+        System.out.println(handledText + " (replace char)");
+
+        // #1
+        words = textHandler.splitIntoWords(text);
+        textHandler.replaceCharAWithOAfterP(words);
+        handledText = textHandler.wordsIntoText(words);
+        System.out.println(handledText + " (replace 'a' with 'o' after 'p')");
+
+        // #1
+        words = textHandler.splitIntoWords(text);
+        textHandler.replaceWordsWithSubstring(wordLengthToReplaceWithSubstring, substring, words);
+        handledText = textHandler.wordsIntoText(words);
+        System.out.println(handledText + " (replace specified length words with substring)");
+
+        // #4
+
+        // #5
+
+        // all
+        handledText = textHandler.applyAllFeatures(
+                text,
+                replacer,
+                indexOfCharToReplace,
+                wordLengthToReplaceWithSubstring,
+                substring
+        );
+
+        System.out.println(handledText + " (all)");
     }
 
 }
--- src/com/pazukdev/TextHandler.java
+++ src/com/pazukdev/TextHandler.java
@@ -11,8 +11,8 @@ import java.util.List;
  * Программа обработки текста, который может быть получен как с консоли, так и с файла.
  * 1. В каждом слове k-ю букву заменить заданным символом. Если k больше длины слова,
  * корректировку не выполнять.
- * 2. В тексте после буквы Р, если она не последняя в слове, ошибочно напечатана буква А
- * вместо О. Внести исправления в текст.
+ * 2. В тексте после буквы Р, если она не последняя в слове, ошибочно напечатана буква А // как определить что ОШИБОЧНО?
+ * вместо О. Внести исправления в текст.                                                 // или любую А менять на О?
  * 3. В тексте слова заданной длины заменить указанной подстрокой, длина которой может
  * не совпадать с длиной слова.
  * 4. Из небольшого текста удалить все символы, кроме пробелов, не являющиеся буквами.
@@ -21,29 +21,59 @@ import java.util.List;
  */
 public class TextHandler {
 
+    // TODO
+    //  1. write implementation for all defined but not implemented methods
+    //  2. define methods for p. #4 and #5 and write its implementations
+    //  3. add input from console feature
+
     private static final String SEPARATOR = " ";
 
-    public String handle(final String text, final char replacer, final int indexOfCharToReplace) {
+    public String applyAllFeatures(final String text,
+                                   final char replacer,
+                                   final int indexOfCharToReplace,
+                                   final int wordLengthToReplaceWithSubstring,
+                                   final String substring) { // add params for #4 and #5 methods
         final List<String> words = splitIntoWords(text);
-        replaceChar(replacer, indexOfCharToReplace, words);
+
+        // #5 here
+        // #4 here
+        replaceWordsWithSubstring(wordLengthToReplaceWithSubstring, substring, words); // #3
+        replaceCharAWithOAfterP(words); // #2
+        replaceChar(replacer, indexOfCharToReplace, words); // #1
 
         final String handledText = wordsIntoText(words);
         return handledText;
     }
 
+    // #1
     public void replaceChar(final char replacer, final int indexOfCharToReplace, final List<String> words) {
         final List<String> changedWords = new ArrayList<>();
         for (final String word : words) {
-            final StringBuilder changedWord = new StringBuilder(word);
             try {
-                changedWord.setCharAt(indexOfCharToReplace, replacer); // try to replace character
-                changedWords.add(changedWord.toString());
+                final String changedWord = replaceChar(replacer, indexOfCharToReplace, word); // try to replace char
+                changedWords.add(changedWord);
             } catch (final StringIndexOutOfBoundsException e) { // but if the word is shorter than specified index
                 changedWords.add(word); // leave the word as is
             }
         }
         words.clear();
         words.addAll(changedWords);
+    }
+
+    // #2
+    public void replaceCharAWithOAfterP(final List<String> words) {
+        // TODO write method implementation
+        // in the loop for every single word:
+        // 1. find all chars after 'P'
+        // 2. in the inner loop: if char == 'A' replace it with 'O'
+        // Achtung! make sure that words in the list are really changed (e.g.: as in replaceChar() method)
+    }
+
+    // #3
+    public void replaceWordsWithSubstring(final int wordLengthToReplaceWithSubstring,
+                                          final String substring,
+                                          final List<String> words) {
+        // TODO write method implementation
     }
 
     public List<String> splitIntoWords(final String text) {
@@ -58,6 +88,17 @@ public class TextHandler {
         }
         text = text.trim(); // to remove space at the end
         return text;
+    }
+
+    // private methods
+
+    // #1
+    private String replaceChar(final char replacer,
+                               final int indexOfCharToReplace,
+                               final String word) throws StringIndexOutOfBoundsException {
+        final StringBuilder changedWord = new StringBuilder(word);
+        changedWord.setCharAt(indexOfCharToReplace, replacer);
+        return changedWord.toString();
     }
 
 }
